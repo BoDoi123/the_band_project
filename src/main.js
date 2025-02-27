cabbea6baa6775d691754975d8c77a2683298310
Chu Long <chulong_t65@hus.edu.vn>
2025-02-28 01:38:15 +0700

    Responsive The Bank Web

--- src/main.js
+++ src/main.js
@@ -7,3 +7,31 @@ function closeModal() {
 function showModal() {
 	modal.style.display = "flex";
 }
+
+// Mobile Event
+// Đóng/Mở Mobile Menu
+let header = document.getElementById("header");
+
+let mobileMenu = document.querySelector(".mobile-menu-btn");
+
+mobileMenu.addEventListener("click", (e) => {
+	header.classList.toggle("show-menu-mobile");
+	sub_menu.classList.remove("show_sub_menu");
+});
+
+// Tự động đóng khi chọn Menu
+let nav = document.querySelector("#nav");
+
+nav.addEventListener("click", (e) => {
+	header.classList.remove("show-menu-mobile");
+});
+
+let sub_menu_btn = document.querySelector(".sub_menu");
+let sub_menu = document.querySelector(".sub_nav");
+
+sub_menu_btn.addEventListener("click", (e) => {
+	e.stopPropagation();
+	e.preventDefault();
+
+	sub_menu.classList.toggle("show_sub_menu");
+});
